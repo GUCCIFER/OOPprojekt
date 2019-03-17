1e1126542fac99c4eca48fa71a2d9ba7abb45a7e
Teet Saar <teetsaar7@gmail.com>
2019-03-17 19:12:27 +0200

    Pahalased ja värgid

--- src/Vangikoobas.java
+++ src/Vangikoobas.java
@@ -21,7 +21,9 @@ public class Vangikoobas {
     }
     public static void Löök(Relv relv, Tegelane tegelane){
         if (kasSaabPihta(relv, tegelane)){
-            tegelane.setElud(tegelane.getElud()-löögiDamage(relv));
+            int kahju = löögiDamage(relv);
+            tegelane.setElud(tegelane.getElud()-kahju);
+            System.out.println(tegelane.getTegelane() + "sai " + kahju + " punkti kahju ja tal on nüüd " + tegelane.getElud() + " elu" );
         }
     }
     public static String sisestus(String message){
@@ -38,10 +40,33 @@ public class Vangikoobas {
     public static void main(String[] args) {
         System.out.println("TUTORIAL");
         List<Tegelane> pahalased = new ArrayList<>();
+        Relv scimmi = new Relv("Scimitar", 2, 5, 3, 3);
         Tegelane peaTegelane = new Tegelane(sisestus("Sisetage oma tegelase nimi"), intSisestus("Sisetage oma tegelase elud"), intSisestus("Sisetage oma tegelase kaitse tase"));
         peaTegelane.setRelv(new Relv( sisestus("Sisetage oma Relva"),intSisestus("Sisetage mitu korda relvaga lööd(1-3)"), intSisestus("Sisetage oma relva tugevus(1-12"),intSisestus("Sisetage oma relva baastugevus(1-3)"),intSisestus("Sisetage oma relva täpsus(1-7)")));
         for (int i = 0; i < intSisestus("Sisestage vastaste arv"); i++) {
-            pahalased.add(new Tegelane(sisestus("Sisetage oma pahalase nimi"), intSisestus("Sisetage pahalase elud"), intSisestus("Sisetage pahalase kaitse tase"), pahalaseRelv()));
+            pahalased.add(new Tegelane(sisestus("Sisetage oma pahalase nimi"), intSisestus("Sisetage pahalase elud"), intSisestus("Sisetage pahalase kaitse tase"), scimmi));
+        }
+        while (pahalased.size() != 0 && peaTegelane.getElud() > 0){
+            String target = sisestus("Keda sa ründad?");
+            for (Tegelane pahalane: pahalased){
+                if (pahalane.getTegelane().equals(target)){
+                    Löök(peaTegelane.getRelv(), pahalane);
+                    if (pahalane.getElud() < 1){
+                        pahalased.remove(pahalane);
+                    }
+                    break;
+                }
+
+            }
+            for (Tegelane pahalane: pahalased){
+                Löök(pahalane.getRelv(), peaTegelane);
+                if (peaTegelane.getElud() < 1){
+                    break;
+                }
+            }
+
+
+
         }
     }
 }
