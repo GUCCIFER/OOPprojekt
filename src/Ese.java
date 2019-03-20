801964ba64d07806947014ca675a806b2d3f7da6
Teet Saar <teetsaar7@gmail.com>
2019-03-20 20:50:46 +0200

    Ese

---
+++ src/Ese.java
@@ -0,0 +1,12 @@
+public class Ese {
+    private String nimi;
+    Ese(String nimi){
+        this.nimi = nimi;
+    }
+    public String toString(){
+        return nimi;
+    }
+    public String getNimi(){
+        return nimi;
+    }
+}
